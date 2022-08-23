9757f636125ce5dd05ed21221954cfbce1572120
Bahiy <bahiy14@gmail.com>
2022-08-23 19:58:58 +0100

    Exercícios da Aula 01 - ALURA

---
+++ type-number.js
@@ -0,0 +1,13 @@
+const meuNumero = 3;
+
+const primeiroNumero =1;
+const segundoNumero = 2;
+
+
+Somar(primeiroNumero, segundoNumero)
+
+
+function Somar(n1, n2){
+    const resultado = n1+n2;
+    console.log(resultado);
+}
